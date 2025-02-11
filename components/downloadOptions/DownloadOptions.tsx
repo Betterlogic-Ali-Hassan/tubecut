8d14668112df879ec14f65cada88ecb9eff4ac6f
HassanThePassionate <maqsoodia777@gmail.com>
2025-02-11 15:49:11 +0500

    Update

--- components/downloadOptions/DownloadOptions.tsx
+++ components/downloadOptions/DownloadOptions.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Options from "./options/Options";
+import Button from "../ui/button/Button";
 const DownloadOptions = () => {
   return (
     <section className='flex flex-col  items-center mt-[40px] max-w-[600px] mx-auto '>
       <h1 className='self-start max-sm:px-6'>Downloads</h1>
       <Options />
+      <Button>Try New Video</Button>
     </section>
   );
 };
